a28b0dbf40383f68aef8cd4939b02c6980a959e7
franclarke <franclarke00@hotmail.com>
2026-03-19 15:22:28 -0300

    fix: update contact email and form endpoint to new address

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,4 +1,4 @@
 export const BOOK_CALL_URL = "/contacto";
 export const SITE_URL = "https://www.underflowlabs.com";
-export const CONTACT_EMAIL = "contacto@underflowlabs.com";
-export const CONTACT_FORM_ENDPOINT = "https://formsubmit.co/ajax/contacto@underflowlabs.com";
+export const CONTACT_EMAIL = "hola@underflowlabs.com";
+export const CONTACT_FORM_ENDPOINT = "https://formsubmit.co/ajax/hola@underflowlabs.com";
